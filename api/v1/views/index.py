e4250a3d297fb4c47965c65ef2c50f28902f2561
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-16 02:36:47 -0300

    d

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module provides a simple get status of the web server."""
 
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status')
 def ret_status():
-    """Return OK status."""
     status = {"status": "OK"}
     return jsonify(status)
+"""
