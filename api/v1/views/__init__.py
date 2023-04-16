0f14fea58637bf5ba3071db27f97512aeefd7065
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-16 02:21:50 -0300

    chan position of import *

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """ module to create app views for blueprint"""
 
 from flask import Blueprint
-from api.v1.views.index import app_views
-
 
 app_views = Blueprint(url_prefix='/api/v1')
+
+from api.v1.views.index import *
