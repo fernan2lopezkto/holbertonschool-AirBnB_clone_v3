7cdbf232f0252f99aed20b5b6db9e2da204e1e48
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-23 14:21:51 -0300

    fgvgg

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module to create app views for blueprint"""
 
-# # from flask import Blueprint
+from flask import Blueprint
 
 # # app_views = Blueprint(url_prefix='/api/v1')
 
