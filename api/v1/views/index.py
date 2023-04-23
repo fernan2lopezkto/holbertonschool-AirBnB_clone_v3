ee1b4423e40b6a0f609f6c23bc0237a94fb3347c
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-23 20:50:43 -0300

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,32 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+from models import storage
+
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
 
 @app_views.route('/status')
 def index():
     """returned a object with json representation"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """return a count object with json representation"""
+    """counter of created objects of each classs"""
+    objects = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(objects)
