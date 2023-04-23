69fd16e1419163cbb22f844b9f4aa4c53cb7d8ee
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-23 13:12:51 -0300

    add documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """
-starts a flask web  app
+This is a Flask application that runs an API with a blueprint for views and closes a storage context
+when the application context is torn down.
 """
 
 from os import getenv
@@ -17,14 +17,22 @@ HBNB_API_PORT = getenv('HBNB_API_PORT')
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def handle_context():
-    """Close sotrage"""
+    """
+    This function closes a storage context.
+    """
     storage.close()
 
+
+# This code block is checking if the current module is being run as the main program (as opposed to
+# being imported as a module into another program). If it is being run as the main program, it tries
+# to run the Flask application with the specified host and port, and with threading enabled. If an
+# exception occurs (such as if the specified host and port are not available), it runs the Flask
+# application with a default host and port.
 if __name__ == "__main__":
     try:
-        app.run(host= HBNB_API_HOST, port= HBNB_API_PORT, threaded=True)
+        app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
     except:
-        app.run(host= '0.0.0.0', port= '5000', threaded= True)
-
+        app.run(host='0.0.0.0', port='5000', threaded=True)
