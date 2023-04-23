9a4184dfc61c23f0c26eae98746f602580c40626
juan_latitude <fernan2lopezkto@gmail.com>
2023-04-23 14:07:18 -0300

    ck

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,30 +4,30 @@ This is a Flask application that runs an API with a blueprint for views and clos
 when the application context is torn down.
 """
 
-from os import getenv
-from flask import Flask
-from api.v1.views import app_views
-from models import storage
+# from os import getenv
+# from flask import Flask
+# from api.v1 import 
+# from models import storage
 
 
-HBNB_API_HOST = getenv('HBNB_API_HOST')
-HBNB_API_PORT = getenv('HBNB_API_PORT')
+# HBNB_API_HOST = getenv('HBNB_API_HOST')
+# HBNB_API_PORT = getenv('HBNB_API_PORT')
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+# app = Flask(__name__)
+# app.register_blueprint(views)
 
 
-@app.teardown_appcontext
-def handle_context():
-    """
-    This function closes a storage context.
-    """
+# @app.teardown_appcontext
+# def handle_context():
+#     """
+#     This function closes a storage context.
+#     """
 
-    storage.close()
+#     storage.close()
 
-if __name__ == "__main__":
-    try:
-        app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
-    except:
-        app.run(host='0.0.0.0', port='5000', threaded=True)
+# if __name__ == "__main__":
+#     try:
+#         app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+#     except:
+#         app.run(host='0.0.0.0', port='5000', threaded=True)
